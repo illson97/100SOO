0d16a44e64be400181c44731e0d49447289ca0db
illson97 <illson97@github.com>
2023-09-27 20:40:26 +0900

    #13 - Data Rest 적용 및 테스트 정의
    Data Rest 서비스하고 api 존재 여부에 대한 테스트 정의

--- src/main/java/spring/soo/repository/ArticleRepository.java
+++ src/main/java/spring/soo/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package spring.soo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import spring.soo.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
